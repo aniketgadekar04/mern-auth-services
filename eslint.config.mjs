b9ad4b747ac3364b9d5770717e5a6f71033c4db0
aniketgadekar04 <156211209+aniketgadekar04@users.noreply.github.com>
2026-06-19 21:40:34 +0530

    lint staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,19 @@
-import js from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig({
-  files: ['**/*.{js,ts}'],
-  ignores: ['dist/**', 'node_modules/**'],
-  extends: [
-    js.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-  ],
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+    files: ['**/*.{js,ts}'],
+    ignores: ['dist/**', 'node_modules/**'],
+    extends: [js.configs.recommended, tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+        parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
     },
-  },
-  rules: {
-    'no-console': 'error',
-    'dot-notation':'error'
-  },
-});
+    rules: {
+        'no-console': 'error',
+        'dot-notation': 'error',
+    },
+})
